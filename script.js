9e55b05344d0ed9d750579072d1d3ad205e0dcff
unknown <dharmielola@gmail.com>
2023-08-10 22:14:53 +0100

    increase arraynumbers

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function userAge(){
 console.log(userAge());
 
 //create an array of numbers
-let input = 23;
-const arrayNumbers = [1,2,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+let input = 45;
+const arrayNumbers = [1,2,2,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45];
 
 //Loop through the array and output only even numbers
 for (let i = 0; i < arrayNumbers.length; i++){
